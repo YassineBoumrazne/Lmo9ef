bcfbf71ba5581cbc048794a1252335734aff4ddd
hakeemkadim <hakeemkadim@gmail.com>
2023-03-07 22:39:56 +0100

    Major changes
    the pom also changed because of the servlet and added some dependencies to it

--- src/main/java/com/example/lmo9ef/LoginServlet.java
+++ src/main/java/com/example/lmo9ef/LoginServlet.java
@@ -4,8 +4,11 @@ import java.io.*;
 
 import com.example.lmo9ef.Model.Client;
 import com.example.lmo9ef.Repository.AuthRepositroy;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
